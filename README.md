187669f58ae2d3b0a8e3eb0b6810e8c84a067b90
drxos <sounton@gmail.com>
2017-05-25 00:10:35 +0100

    ui: layout footer

---
+++ README.md
@@ -0,0 +1,1 @@
+# SountonMe Express Project
--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 exports.run = function(app){
   app.locals.site = 'SountonME'
   app.locals.current = new Date().getFullYear()
+  app.locals.description = 'geomatic, géomatique, benin, javascript, node, express, modelisation'
+  app.locals.author = 'Hodonou Sounton'
 }
